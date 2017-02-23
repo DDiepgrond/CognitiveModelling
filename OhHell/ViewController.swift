e6595853b391a1e331a87e58cb0add1acded4efe
D. Diepgrond <s2393131@ai178140.ai.rug.nl>
2017-02-23 16:24:40 +0100

    test button

--- OhHell/ViewController.swift
+++ OhHell/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchButton(_ sender: UIButton) {
+        NSLog("Test succeeded")
+    }
 
 }
 
